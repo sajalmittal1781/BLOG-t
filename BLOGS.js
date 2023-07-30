f275cda321ee4e271ca54c26475e9ee8a6db01d5
SAJAL <75297054+sajalmittal1781@users.noreply.github.com>
2023-07-30 15:59:32 +0530

    Update BLOGS.js

--- BLOGS.js
+++ BLOGS.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBF6_sui0kgG6A7WwBtj0F5WPiWitdSHAg",
-  authDomain: "blog-t-b3345.firebaseapp.com",
-  projectId: "blog-t-b3345",
-  storageBucket: "blog-t-b3345.appspot.com",
-  databaseURL:"https://blog-t-b3345-default-rtdb.firebaseio.com/",
-  messagingSenderId: "716717083227",
-  appId: "1:716717083227:web:3de1c4d058eb7288dc0d7e"
+  apiKey: "AIzaSyAJOo9RL7kg7YgJ3vGrCclLEfo63-gz_4A",
+  authDomain: "blogit-bd237.firebaseapp.com",
+  databaseURL: "https://blogit-bd237-default-rtdb.firebaseio.com",
+  projectId: "blogit-bd237",
+  storageBucket: "blogit-bd237.appspot.com",
+  messagingSenderId: "1078183903757",
+  appId: "1:1078183903757:web:6401270b22a86e17264b44",
+  measurementId: "G-BHPXWHQ0RN"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
